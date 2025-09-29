9157f5e99ecb58e2185e3386b5f278cc0e7fa1a0
Ntarekp <kpntare@gmail.com>
2025-09-29 16:13:59 +0200

    creating a custom component

---
+++ 03 React Essentials/essentials/src/App.jsx
@@ -0,0 +1,24 @@
+function Header(){
+  return(
+    <header>
+        <img src="#images" alt="image" />
+        <h1>React Essentials</h1>
+        <p>
+          Fundamental React concepts you will need for almost any app you are going to build
+        </p>
+      </header>
+  )
+}
+
+function App(){
+  return (
+    <div>
+      
+       <main>
+        <h2>
+          Time to get Started!
+        </h2>
+       </main>
+    </div>
+  )
+}
