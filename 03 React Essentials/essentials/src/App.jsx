21d34badaca8c1e7b018652b96d9e7492434cc3a
Ntarekp <kpntare@gmail.com>
2025-09-30 15:58:16 +0200

    Added more files regarding props

--- 03 React Essentials/essentials/src/App.jsx
+++ 03 React Essentials/essentials/src/App.jsx
@@ -1,8 +1,8 @@
 import reactImg from './assets/react.svg';
-const reactDescriptions = ["Fundamental", "Crucial","Core"]
+const reactDescriptions = ["Fundamental", "Crucial", "Core"]
 
-function genRandomInt(max){
-  return Math.floor(Math.random()*(max +1));
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
 }
 function Header() {
   const description = reactDescriptions[genRandomInt(2)]
@@ -11,21 +11,40 @@ function Header() {
       <img src={reactImg} alt="image" />
       <h1>React Essentials</h1>
       <p>
-      {description}   React concepts you will need for almost any app you are going to build
+        {description}   React concepts you will need for almost any app you are going to build
       </p>
     </header>
   )
 }
 
+function CoreConcepts(props) {
+  return <li>
+    <img src={props.image } alt={props.title} />
+    <h3>{props.title}</h3>
+    <p>{props.description} </p>
+  </li>
+}
 
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>
-          Time to get Started!
-        </h2>
+        <section className="coreConcepts">
+          <h2>
+            Time to get Started!
+          </h2>
+          <ul>
+            <CoreConcepts title="Componenents" description="The core UI building block"
+              image  ={reactImg} />
+            <CoreConcepts />
+            <CoreConcepts />
+            <CoreConcepts />
+
+          </ul>
+        </section>
+
+
       </main>
     </div>
   )
