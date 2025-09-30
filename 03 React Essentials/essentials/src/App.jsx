7c4eb6ea9ca3b4d240c60b51e64e7f5f5986f808
Ntarekp <kpntare@gmail.com>
2025-09-30 16:44:06 +0200

    Using shorter methods on props and also doing prop distructuring

--- 03 React Essentials/essentials/src/App.jsx
+++ 03 React Essentials/essentials/src/App.jsx
@@ -20,11 +20,11 @@ function Header() {
   )
 }
 
-function CoreConcepts(props) {
+function CoreConcepts({image, title, description}) {
   return <li>
-    <img src={props.image} alt={props.title} />
-    <h3>{props.title}</h3>
-    <p>{props.description} </p>
+    <img src={image} alt={title} />
+    <h3>{title}</h3>
+    <p>{description} </p>
   </li>
 }
 
@@ -43,22 +43,11 @@ function App() {
               description={CORE_CONCEPTS[0].description}
               image={CORE_CONCEPTS[0].image} 
               />
-            <CoreConcepts
-              title={CORE_CONCEPTS[1].title}
-              description={CORE_CONCEPTS[1].description}
-              image={CORE_CONCEPTS[1].image} 
-              />
+            <CoreConcepts {...CORE_CONCEPTS[1]} />
+            <CoreConcepts {...CORE_CONCEPTS[2]} />
+            <CoreConcepts {...CORE_CONCEPTS[3]} />
 
-            <CoreConcepts
-              title={CORE_CONCEPTS[2].title}
-              description={CORE_CONCEPTS[2].description}
-              image={CORE_CONCEPTS[2].image} 
-              />
-               <CoreConcepts
-              title={CORE_CONCEPTS[3].title}
-              description={CORE_CONCEPTS[3].description}
-              image={CORE_CONCEPTS[3].image} 
-              />
+           
           </ul>
         </section>
 
